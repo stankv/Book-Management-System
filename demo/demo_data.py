339d065754311439ff7b78c716a591a0754b1180
stankv <stankv@mail.ru>
2026-03-04 13:42:48 +0300

    add demo data

---
+++ demo/demo_data.py
@@ -0,0 +1,20 @@
+from src.models.book import Book
+
+book1 = Book(
+    title="Book 1",
+    author="Author1",
+    year=1999,
+    isbn="ISBN 978-5-6044166-1-7",
+)
+book2 = Book(
+    title="Book 2",
+    author="Author2",
+    year=2004,
+    isbn="ISBN 978-5-6032166-2-6",
+)
+book3 = Book(
+    title="Book 3",
+    author="Author3",
+    year=2008,
+    isbn="ISBN 978-5-6082166-3-5",
+)
