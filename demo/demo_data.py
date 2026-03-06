7e69ce9932a6a557714ad702ebc651252489fb55
stankv <stankv@mail.ru>
2026-03-06 18:09:14 +0300

    add creating demo data (/src/data/books.json)

--- demo/demo_data.py
+++ demo/demo_data.py
@@ -1,4 +1,9 @@
+from pathlib import Path
+
 from src.models.book import Book
+
+from src.services.entity_service import EntityService
+from src.storage.json_storage import JsonStorage
 
 book1 = Book(
     title="Book 1",
@@ -18,3 +23,16 @@ book3 = Book(
     year=2008,
     isbn="ISBN 978-5-6082166-3-5",
 )
+
+def main():
+    # создание json-файла с демо данными
+    data_dir = Path(__file__).parent.parent / "src" / "data"
+    books_storage = JsonStorage(data_dir / "books.json")
+    books_service = EntityService(entity_type=Book, storage=books_storage)
+    books_service.add(book1)
+    books_service.add(book2)
+    books_service.add(book3)
+    print("file books.json with demo data created!")
+
+if __name__ == "__main__":
+    main()
--- src/main.py
+++ src/main.py
@@ -13,18 +13,4 @@ def main():
     manager.run()
 
 if __name__ == "__main__":
-
-    # Загрузка демо-данных и создание json-файла
-    # from src.models.book import Book
-    # from src.services.entity_service import EntityService
-    # from src.storage.json_storage import JsonStorage
-    # from demo.demo_data import book1, book2, book3
-    #
-    # data_dir = Path(__file__).parent / "data"
-    # books_storage = JsonStorage(data_dir / "books.json")
-    # books_service = EntityService(entity_type=Book, storage=books_storage)
-    # books_service.add(book1)
-    # books_service.add(book2)
-    # books_service.add(book3)
-
     main()
